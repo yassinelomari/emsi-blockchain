024ec6f73d0ebc0fc664c06f64bb52fddcc4163b
yassinelomari <yassinelomari95@gmail.com>
2021-04-09 21:30:29 +0100

    First task first test

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,12 +1,13 @@
-/*
- *this file is the header of tasks of Blockchain - crypto
- */
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HEADER_SHA256
+#define HEADER_SHA256
 /**
- *This function calculate the hash of *@s: witch has @len like length and
- *store it in @digest
+ * sha256 - function calculate the hash of a data
+ * @s: the data which we want to calculate the hash value
+ * @len: the lenght of the data
+ * @digest: the hash value
+ * Return: the hash value
 */
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest
 [SHA256_DIGEST_LENGTH]);
 #endif
+

8ef8f529af8f98f1c91fd668113711f685b0997d
yassinelomari <yassinelomari95@gmail.com>
2021-04-17 13:56:10 +0000

    Task1 crypto test1

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -14,7 +14,10 @@
 */
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest
 [SHA256_DIGEST_LENGTH]);
-
+/**
+ * ec_create - function generate private and the public keys
+ * Return: the EC_KEY structur containing both the public and private keys
+*/
 EC_KEY *ec_create(void);
 #endif
 

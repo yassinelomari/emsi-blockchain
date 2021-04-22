51cd4846d4414b23384faa4a9cb1ccf7133900b3
yassinelomari <yassinelomari95@gmail.com>
2021-04-22 15:09:44 +0000

    check5 until task 5

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,5 +1,10 @@
 #include "hblk_crypto.h"
-
+/**
+ * ec_to_pub - function generate private and the public keys
+ * @key: the EC_KEY contaning the public key
+ * @pub: public key
+ * Return: the uint8_t pointor to public key
+*/
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
 	const EC_POINT *my_point;
@@ -7,11 +12,9 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 
 	if (key == NULL || pub == NULL)
 		return (NULL);
-	my_point = malloc(sizeof(my_point));
 	my_point = EC_KEY_get0_public_key(key);
 	if (my_point == NULL)
 		return (NULL);
-	my_grp = malloc(sizeof(my_grp));
 	my_grp = EC_KEY_get0_group(key);
 	if (my_grp == NULL)
 		return (NULL);

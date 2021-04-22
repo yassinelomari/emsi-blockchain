8ede1033c95e25099dcb4853cbec52f82097ca21
yassinelomari <yassinelomari95@gmail.com>
2021-04-22 14:40:26 +0000

    check2 until task 5

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -8,7 +8,11 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 	if (key == NULL)
 		return (NULL);
 	my_point = EC_KEY_get0_public_key(key);
+	if (my_point == NULL)
+		return (NULL);
 	my_grp = EC_KEY_get0_group(key);
+	if (my_grp == NULL)
+		return (NULL);
 	if (!EC_POINT_point2oct(my_grp, my_point,
 		POINT_CONVERSION_UNCOMPRESSED, pub, EC_PUB_LEN, NULL))
 		return (NULL);

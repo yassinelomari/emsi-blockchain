3c6b96ce9c6b30e6b156c8f21a62c49fb12f8e4c
yassinelomari <yassinelomari95@gmail.com>
2021-05-08 10:49:19 +0000

    test1 tache2 mining

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -167,4 +167,13 @@ int block_is_valid(block_t const *block, block_t const *prev_block);
  */
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 	uint32_t difficulty);
+/**
+ * block_mine - a function that mines
+ * a Block in order to insert it in the Blockchain
+ * it finds a hash for block that matches its difficulty
+ * @block: points to the Block to be mined
+ *
+ * Void
+ */
+void block_mine(block_t *block);
 #endif

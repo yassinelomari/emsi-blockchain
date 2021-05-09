a1036014758003520851a765af7b307390253363
yassinelomari <yassinelomari95@gmail.com>
2021-05-09 14:41:37 +0000

    test1 tache12 transaction

--- blockchain/v0.3/block_hash.c
+++ blockchain/v0.3/block_hash.c
@@ -1,16 +1,43 @@
 #include "blockchain.h"
 
 /**
- * block_hash - function compute the block hash
- *
- * @block: the bloch to be hashed
- * @hash_buf: the hash of the block
- * Return: the hash of the block
+ * hash_tx_ids - llist action func to hash outputs
+ * @node: transaction_t* struct
+ * @idx: index of node
+ * @arg: pointer to address to write to
+ * Return: 0 if success else 1
+ */
+int hash_tx_ids(llist_node_t node, unsigned int idx, void *arg)
+{
+	memcpy(*(uint8_t **)arg, ((transaction_t *)node)->id, SHA256_DIGEST_LENGTH);
+	*(uint8_t **)arg += SHA256_DIGEST_LENGTH;
+	return (0);
+	(void)idx;
+}
+
+
+/**
+ * block_hash - computes hash of block
+ * @block: pointer to block to hash
+ * @hash_buf: buffer to store hash/digest
+ * Return: pointer to buffer
  */
 uint8_t *block_hash(block_t const *block,
-		uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+	uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	SHA256((const unsigned char *)&(block->info),
-		4 + 4 + 8 + 8 + 32 + block->data.len, hash_buf);
+	size_t len0 = sizeof(block->info) + block->data.len, len;
+	int8_t *_buf, *buf;
+
+	len = len0;
+	if (llist_size(block->transactions) > 0)
+		len += llist_size(block->transactions) * SHA256_DIGEST_LENGTH;
+	buf = _buf = calloc(1, len);
+	if (!buf)
+		return (NULL);
+	memcpy(buf, &block->info, len0);
+	buf += len0;
+	llist_for_each(block->transactions, hash_tx_ids, &buf);
+	sha256(_buf, len, hash_buf);
+	free(_buf);
 	return (hash_buf);
 }
